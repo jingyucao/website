f971b7e0be3ca0d0a4fa4491c94a31490d83ab08
Jingyu Cao <jingyu.cao@jonasundderwolf.de>
2025-09-03 15:40:53 +0200

    fix: declare type for import

--- src/components/welcome/Welcome.tsx
+++ src/components/welcome/Welcome.tsx
@@ -1,40 +1,53 @@
 import background from './image/background.jpg';
-import classes from './Welcome.module.css';
 import linkedinLogo from './image/linkedin-logo.png';
 import linkedinBlueLogo from './image/linkedin-blue.png';
 import githubLogo from './image/github-icon.png';
 import githubGirlLogo from './image/github-girl.png';
 
-const Welcome = () => {
-    return (
-        <div className={classes.welcome}>
-            <img src={background} className={classes.bgImg}/>
-            <div className={classes.introduction}>
-                <div className={classes.middleFont}>Hello World, I am</div>
-                <div className={classes.bigFont}>JINGYU CAO</div>
-                <div className={classes.smallFont}>I am a Self-taught Frontend Developer.<br/>I build websites with a focus on responsiveness, accessibility and pleasing aesthetics.</div>
-            </div>
-            <div className={classes.logo}>
-                <a href="https://www.linkedin.com/in/jingyu-cao-7457a4169/">
-                    <div className={classes.logoCard} data-testid='logoCard'>
-                        <img className={classes.linkedinLogoItem} src={linkedinLogo} alt="Linkedin"
-                             data-testid='whiteLinkedinLogo'/>
-                        <img className={classes.linkedinLogoTop} src={linkedinBlueLogo} alt="Linkedin"
-                             data-testid='colorLinkedinLogo'/>
-                    </div>
-                </a>
-                <a href="https://github.com/jingyucao">
-                    <div className={classes.logoCard}>
-                        <img className={classes.githubLogoItem} src={githubLogo} alt="Github"
-                             data-testid='whiteGithubLogo'/>
-                        <img className={classes.githubLogoTop} src={githubGirlLogo} alt="Github"
-                             data-testid='colorGithubLogo'/>
-                    </div>
-                </a>
+import classes from './Welcome.module.css';
 
-            </div>
+const Welcome = () => (
+  <div className={classes.welcome}>
+    <img src={background} className={classes.bgImg} />
+    <div className={classes.introduction}>
+      <div className={classes.middleFont}>Hello World, I am</div>
+      <div className={classes.bigFont}>JINGYU CAO</div>
+    </div>
+    <div className={classes.logo}>
+      <a href="https://www.linkedin.com/in/jingyu-cao-7457a4169/">
+        <div className={classes.logoCard} data-testid="logoCard">
+          <img
+            className={classes.linkedinLogoItem}
+            src={linkedinLogo}
+            alt="Linkedin"
+            data-testid="whiteLinkedinLogo"
+          />
+          <img
+            className={classes.linkedinLogoTop}
+            src={linkedinBlueLogo}
+            alt="Linkedin"
+            data-testid="colorLinkedinLogo"
+          />
         </div>
-    )
-}
+      </a>
+      <a href="https://github.com/jingyucao">
+        <div className={classes.logoCard}>
+          <img
+            className={classes.githubLogoItem}
+            src={githubLogo}
+            alt="Github"
+            data-testid="whiteGithubLogo"
+          />
+          <img
+            className={classes.githubLogoTop}
+            src={githubGirlLogo}
+            alt="Github"
+            data-testid="colorGithubLogo"
+          />
+        </div>
+      </a>
+    </div>
+  </div>
+);
 
 export default Welcome;
