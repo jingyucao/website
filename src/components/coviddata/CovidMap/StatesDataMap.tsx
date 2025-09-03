9b2533f7566c4ee8b8cccd8b59a66cd798d7fefa
Jingyu Cao <jingyu.cao@jonasundderwolf.de>
2025-09-03 17:41:53 +0200

    feat: add basic files for storybook

--- src/components/coviddata/CovidMap/StatesDataMap.tsx
+++ src/components/coviddata/CovidMap/StatesDataMap.tsx
@@ -1,54 +1,67 @@
-import {useEffect, useRef} from "react";
+import { useEffect, useRef } from 'react';
+import * as echarts from 'echarts/core';
 import 'echarts/lib/component/title';
 import 'echarts/lib/component/tooltip';
 import 'echarts/lib/component/grid';
 import 'echarts/lib/chart/line';
 import 'echarts/lib/component/visualMap';
 import 'echarts/lib/component/geo';
-import {MapChart} from 'echarts/charts';
-import geoJson from "./germanymap.json";
+import { MapChart } from 'echarts/charts';
+import {
+  TitleComponent,
+  TooltipComponent,
+  GridComponent,
+  VisualMapComponent,
+  GeoComponent,
+} from 'echarts/components';
+import { CanvasRenderer } from 'echarts/renderers';
+
+import geoJson from './germanymap.json';
 import classes from './StatesData.module.css';
 
 export interface chartProps {
-    options: any
+  options: any;
 }
-
-let echarts = require('echarts/lib/echarts');
-
-echarts.use([MapChart]);
-echarts.registerMap("germany", {geoJSON: geoJson});
+echarts.use([
+  MapChart,
+  TitleComponent,
+  TooltipComponent,
+  GridComponent,
+  VisualMapComponent,
+  GeoComponent,
+  CanvasRenderer,
+]);
+echarts.registerMap('germany', { geoJSON: geoJson });
 
 const StatesDataMap = (props: chartProps) => {
+  const chartRef = useRef(null) as any;
+  let covidMapChart = null as any;
 
-    const chartRef = useRef(null) as any;
-    let covidMapChart = null as any;
+  const renderChart = () => {
+    const chart = echarts.getInstanceByDom(chartRef.current);
+    if (chart) {
+      covidMapChart = chart;
+    } else {
+      covidMapChart = echarts.init(chartRef.current);
+      window.addEventListener('resize', () => {
+        covidMapChart && covidMapChart.resize();
+      });
+    }
+    covidMapChart.setOption(props.options);
+  };
 
-    const renderChart = () => {
-        const chart = echarts.getInstanceByDom(chartRef.current)
-        if (chart) {
-            covidMapChart = chart;
-        } else {
-            covidMapChart = echarts.init(chartRef.current);
-            window.addEventListener('resize', () => {
-                covidMapChart && covidMapChart.resize();
-            })
-        }
-        covidMapChart.setOption(props.options)
+  useEffect(() => {
+    renderChart();
+    return () => {
+      covidMapChart && covidMapChart.dispose();
     };
+  });
 
-    useEffect(() => {
-            renderChart()
-            return () => {
-                covidMapChart && covidMapChart.dispose()
-            }
-        }
-    )
-
-    return (
-        <div className={classes.mapContainer}>
-            <div className={classes.map} ref={chartRef} id='map'/>
-        </div>
-    )
-}
+  return (
+    <div className={classes.mapContainer}>
+      <div className={classes.map} ref={chartRef} id="map" />
+    </div>
+  );
+};
 
 export default StatesDataMap;
--- src/components/coviddata/IncidenceRate/IncidenceRateTabel.tsx
+++ src/components/coviddata/IncidenceRate/IncidenceRateTabel.tsx
@@ -1,107 +1,108 @@
-import {useEffect, useRef} from "react";
-import classes from "./IncidenceRate.module.css";
+import { useEffect, useRef } from 'react';
+import * as echarts from 'echarts/core';
+import classes from './IncidenceRate.module.css';
 
 interface chartProps {
-    data: any
+  data: any;
 }
 
-let echarts = require('echarts/lib/echarts');
+echarts.use([]);
 
 const IncidenceRateTabel = (props: chartProps) => {
+  const chartRef = useRef(null) as any;
+  let covidIncidenceChart = null as any;
 
-    const chartRef = useRef(null) as any;
-    let covidIncidenceChart = null as any;
+  let arrOfDate = props.data.map((a: any) => a.date.substr(0, 10));
+  let arrOfIncidence = props.data.map((a: any) => a.weekIncidence.toFixed(2));
 
-    let arrOfDate = props.data.map((a: any) => a.date.substr(0, 10));
-    let arrOfIncidence = props.data.map((a: any) => a.weekIncidence.toFixed(2));
+  const options = {
+    grid: {
+      bottom: 50,
+      left: 50,
+    },
+    tooltip: {
+      trigger: 'axis',
+    },
+    xAxis: {
+      type: 'category',
+      data: arrOfDate,
+      axisLine: {
+        show: 'true',
+        lineStyle: {
+          color: '#666',
+          width: 2,
+          cap: 'square',
+        },
+      },
+      axisLabel: {
+        fontFamily: '"Work Sans", sans-serif',
+        align: 'center',
+        margin: '12',
+      },
+      axisPointer: {
+        type: 'shadow',
+      },
+    },
+    yAxis: {
+      // name: 'Y-Axis',
+      // nameLocation: 'middle',
+      type: 'value',
+      axisLine: {
+        show: 'true',
+        lineStyle: {
+          color: '#666',
+          width: 2,
+          cap: 'square',
+        },
+      },
+      axisLabel: {
+        fontFamily: '"Work Sans", sans-serif',
+        align: 'right',
+        margin: '12',
+      },
+      splitLine: {
+        lineStyle: {
+          type: 'dotted',
+        },
+      },
+    },
+    series: [
+      {
+        data: arrOfIncidence,
+        type: 'line',
+        name: 'Incidence Rate',
+        areaStyle: {
+          color: 'rgba(66, 165, 245, 0.5)',
+        },
+      },
+    ],
+  };
 
-    const options = {
-        grid: {
-            bottom: 50,
-            left: 50
-        },
-        tooltip: {
-            trigger: 'axis'
-        },
-        xAxis: {
-            type: 'category',
-            data: arrOfDate,
-            axisLine: {
-                show: 'true',
-                lineStyle: {
-                    color: '#666',
-                    width: 2,
-                    cap: 'square'
-                }
-            },
-            axisLabel: {
-                fontFamily: '"Work Sans", sans-serif',
-                align: 'center',
-                margin: '12',
-            },
-            axisPointer: {
-                type: 'shadow'
-            },
-
-        },
-        yAxis: {
-            // name: 'Y-Axis',
-            // nameLocation: 'middle',
-            type: 'value',
-            axisLine: {
-                show: 'true',
-                lineStyle: {
-                    color: '#666',
-                    width: 2,
-                    cap: 'square'
-                }
-            },
-            axisLabel: {
-                fontFamily: '"Work Sans", sans-serif',
-                align: 'right',
-                margin: '12'
-            },
-            splitLine: {
-                lineStyle: {
-                    type: 'dotted',
-                }
-            }
-        },
-        series: [{
-            data: arrOfIncidence,
-            type: 'line',
-            name: 'Incidence Rate',
-            areaStyle: {
-                color: 'rgba(66, 165, 245, 0.5)'
-            }
-        }]
+  const renderChart = () => {
+    const chart = echarts.getInstanceByDom(chartRef.current);
+    if (chart) {
+      covidIncidenceChart = chart;
+    } else {
+      covidIncidenceChart = echarts.init(chartRef.current);
+      window.addEventListener('resize', () => {
+        covidIncidenceChart && covidIncidenceChart.resize();
+      });
     }
+    covidIncidenceChart.setOption(options);
+  };
 
-    const renderChart = () => {
-        const chart = echarts.getInstanceByDom(chartRef.current)
-        if (chart) {
-            covidIncidenceChart = chart
-        } else {
-            covidIncidenceChart = echarts.init(chartRef.current)
-            window.addEventListener('resize', () => {
-                covidIncidenceChart && covidIncidenceChart.resize();
-            })
-        }
-        covidIncidenceChart.setOption(options)
+  useEffect(() => {
+    renderChart();
+    return () => {
+      covidIncidenceChart && covidIncidenceChart.dispose();
     };
+  });
 
-    useEffect(() => {
-        renderChart()
-        return () => {
-            covidIncidenceChart && covidIncidenceChart.dispose()
-        }
-    })
-
-    return (
-        <div className={classes.rateContainer}>
-            <div className={classes.incidence} ref={chartRef} id='incidence'/>
-        </div>
-    )
-}
+  return (
+    <div className={classes.rateContainer}>
+      <div className={classes.incidence} ref={chartRef} id="incidence" />
+    </div>
+  );
+};
 
 export default IncidenceRateTabel;
